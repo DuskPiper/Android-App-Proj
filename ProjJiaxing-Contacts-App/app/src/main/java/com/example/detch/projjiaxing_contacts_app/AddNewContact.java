f869aa1bd4c433dcdecad0493b43f3f80a08a1e8
DuskPiper <DuskPiper@users.noreply.github.com>
2018-10-15 00:37:00 -0400

    projJiaxing
    a contact list

---
+++ ProjJiaxing-Contacts-App/app/src/main/java/com/example/detch/projjiaxing_contacts_app/AddNewContact.java
@@ -0,0 +1,116 @@
+package com.example.detch.projjiaxing_contacts_app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AddNewContact extends Activity {
+    Button addContact;
+    EditText newName;
+    EditText newPhone;
+    ListView newRelationships;
+    ArrayList<String> names;
+    ArrayList<String> newRelationshipNames;
+    ArrayList<Map<String,String>> liteContactBook;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_contact);
+
+        newRelationshipNames=new ArrayList<String>();
+        addContact=(Button)findViewById(R.id.addNewContactButton);
+        newName=(EditText)findViewById(R.id.addNewNameBox);
+        newPhone=(EditText)findViewById(R.id.addNewPhoneBox);
+        newRelationships=(ListView)findViewById(R.id.addNewRelationshipList);
+        names=this.getIntent().getExtras().getStringArrayList("names");
+        liteContactBook=new ArrayList<Map<String,String>>();
+        for(int i=0;i<names.size();i++){
+            Map<String,String> person=new HashMap<String,String>();
+            person.put("name",names.get(i));
+            liteContactBook.add(person);
+        }
+        AddRelationshipAdapter addRelationshipAdapter=new AddRelationshipAdapter(AddNewContact.this,
+                liteContactBook,R.id.adderCheckbox,R.layout.relationship_with_checkbox_per_item,new String[]{"name"},new int[]{R.id.addRelationshipName});
+        newRelationships.setAdapter(addRelationshipAdapter);
+        addRelationshipAdapter.notifyDataSetChanged();
+
+        addContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String addedName=newName.getText().toString();
+                String addedPhone=newPhone.getText().toString();
+                if(addedName.length()<1){
+                    Toast.makeText(AddNewContact.this.getApplicationContext(),
+                            "Plz fill the name",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Bundle sendBackNewContact =new Bundle();
+                sendBackNewContact.putStringArrayList("relationship",newRelationshipNames);
+                sendBackNewContact.putString("name",addedName);
+                sendBackNewContact.putString("phone",addedPhone);
+                Intent addedContact=new Intent();
+                addedContact.putExtras(sendBackNewContact);
+                AddNewContact.this.setResult(1111,addedContact);
+                AddNewContact.this.finish();
+            }
+        });
+    }
+
+
+
+    public class AddRelationshipAdapter extends SimpleAdapter {
+        List<Map<String, String>> listmaps;
+        private int myCheckBoxId;
+
+        public AddRelationshipAdapter(Context context, List<Map<String, String>> list, int checkBoxId, int resource, String[] from, int[] to) {
+            super(context, list, resource, from, to);
+            this.listmaps = list;
+            this.myCheckBoxId=checkBoxId;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            View view = super.getView(position, convertView, parent);
+            CheckBox deleter = (CheckBox) view.findViewById(R.id.deleterBox);
+            deleter.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    String checkedName=AddNewContact.this.liteContactBook.get(position).get("name");
+                    if(isChecked){
+                        AddNewContact.this.newRelationshipNames.add(checkedName);
+                    }
+                    else{
+                        AddNewContact.this.newRelationshipNames.remove(checkedName);
+                    }
+                    notifyDataSetChanged();
+                }
+            });
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return listmaps.size();
+        }
+    }
+
+}
---
+++ ProjJiaxing-Contacts-App/app/src/main/java/com/example/detch/projjiaxing_contacts_app/ContactsList.java
@@ -0,0 +1,213 @@
+package com.example.detch.projjiaxing_contacts_app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.*;
+
+
+public class ContactsList extends AppCompatActivity {
+    Button del_button;
+    Button add_button;
+    ListView main_list;
+    /*Map<String, String> contacts = new HashMap();
+    Map<String, String> relationships = new HashMap();*/
+    List<Map<String,String >> contactbook = new ArrayList<Map<String,String >>();//contacts and their relationships are saved here
+    static List<Integer> to_delete = new ArrayList<Integer>();
+    ContactListMainListAdapter contactListAdapter;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts_list);
+
+
+        add_button = (Button) findViewById(R.id.addButton);
+        del_button = (Button) findViewById(R.id.delButton);
+        main_list = (ListView) findViewById(R.id.listOfContacts);
+        loadData(ContactsList.this);
+        contactListAdapter = new ContactListMainListAdapter(
+                ContactsList.this,contactbook,R.id.deleterBox,R.layout.contact_list_per_item,new String[]{"name"},new int[]{R.id.contactName});
+        main_list.setAdapter(contactListAdapter);
+        contactListAdapter.notifyDataSetChanged();
+
+
+        del_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for(int i=to_delete.size()-1;i>=0;i--){
+                    int now_delete=to_delete.get(i);
+                    contactbook.remove(now_delete);
+                    Log.e("contacts size",Integer.toString(contactbook.size()));
+                }
+                contactListAdapter.notifyDataSetChanged();
+                to_delete.clear();
+            }
+        });
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<String> names=new ArrayList<String>();
+                for(int i=0;i<contactbook.size();i++){
+                    String name=contactbook.get(i).get("name");
+                    names.add(name);
+                }
+                Bundle sendnames=new Bundle();
+                sendnames.putStringArrayList("names",names);
+                Intent toAddNewContact=new Intent(ContactsList.this,AddNewContact.class);
+                toAddNewContact.putExtras(sendnames);
+                startActivityForResult(toAddNewContact,100);//+new contact
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        saveData(ContactsList.this);
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        saveData(ContactsList.this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        //super.onActivityResult();
+        //here we unload data
+        //String result=data.getExtras().get
+        Bundle newlyAdded=data.getExtras();
+        String newName=newlyAdded.getString("name");
+        String newPhone=newlyAdded.getString("phone");
+        ArrayList<String> newRelationship=newlyAdded.getStringArrayList("relationship");
+        StringBuffer relationshipBuffer=new StringBuffer();
+        for(int i=0;i<newRelationship.size();i++){
+            relationshipBuffer.append(newRelationship.get(i));
+            relationshipBuffer.append(";");
+        }
+        relationshipBuffer.deleteCharAt(relationshipBuffer.length()-1);
+        Map<String,String> newPerson=new HashMap<String, String>();
+        newPerson.put("name",newName);
+        newPerson.put("phone",newPhone);
+        newPerson.put("relationships",newRelationship.toString());
+        this.contactbook.add(newPerson);
+        Log.e("Activity Jump","Added new contact");
+        Toast.makeText(ContactsList.this.getApplicationContext(),
+                "Added "+newName,Toast.LENGTH_SHORT).show();
+        this.contactListAdapter.notifyDataSetChanged();
+        
+    }
+
+
+
+    public void saveData(Context context) {
+        //save contactbook(contacts and relationships)
+        List<Map<String, String>> data=this.contactbook;
+        String key="saves";
+        JSONArray mJsonArray = new JSONArray();
+        for (int i = 0; i < data.size(); i++) {
+            Map<String, String> itemMap = data.get(i);
+            Iterator<Map.Entry<String, String>> iterator = itemMap.entrySet().iterator();
+            JSONObject object = new JSONObject();
+            while (iterator.hasNext()) {
+                Map.Entry<String, String> entry = iterator.next();
+                try {
+                    object.put(entry.getKey(), entry.getValue());
+                } catch (JSONException e) { }
+            }
+            mJsonArray.put(object);
+        }
+        SharedPreferences sp = context.getSharedPreferences("finals", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(key, mJsonArray.toString());
+        editor.commit();
+    }
+
+    public void loadData(Context context) {
+        //restore contactbook(contacts and relationships)
+        String key="saves";
+        List<Map<String, String>> data = new ArrayList<Map<String, String>>();
+        SharedPreferences sp = context.getSharedPreferences("finals", Context.MODE_PRIVATE);
+        String result = sp.getString(key, "");
+        try {
+            JSONArray array = new JSONArray(result);
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject itemObject = array.getJSONObject(i);
+                Map<String, String> itemMap = new HashMap<String, String>();
+                JSONArray names = itemObject.names();
+                if (names != null) {
+                    for (int j = 0; j < names.length(); j++) {
+                        String name = names.getString(j);
+                        String value = itemObject.getString(name);
+                        itemMap.put(name, value);
+                    }
+                }
+                data.add(itemMap);
+            }
+        } catch (JSONException e) { }
+        this.contactbook=data;
+
+
+    }
+
+    public class ContactListMainListAdapter extends SimpleAdapter {
+        List<Map<String, String>> listmaps;
+        private int myCheckBoxId;
+
+        public ContactListMainListAdapter(Context context, List<Map<String, String>> list,int checkBoxId, int resource, String[] from, int[] to) {
+            super(context, list, resource, from, to);
+            this.listmaps = list;
+            this.myCheckBoxId=checkBoxId;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            View view = super.getView(position, convertView, parent);
+            CheckBox deleter = (CheckBox) view.findViewById(R.id.deleterBox);
+            deleter.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if(isChecked){
+                        ContactsList.to_delete.add(position);
+                        Collections.sort(ContactsList.to_delete);
+                    }
+                    else{
+                        int index=ContactsList.to_delete.indexOf(position);
+                        if(index>-1){
+                            ContactsList.to_delete.remove(position);
+                        }
+                    }
+                }
+            });
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return listmaps.size();
+        }
+    }
+
+}
+
