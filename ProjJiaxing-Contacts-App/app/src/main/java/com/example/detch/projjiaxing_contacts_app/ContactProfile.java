66016c84c35253a4bac13b644f2856a43ef73e3a
DuskPiper <DuskPiper@users.noreply.github.com>
2018-10-21 21:34:48 -0400

    Moved photo data from shared preference to SQLite database

--- ProjJiaxing-Contacts-App/app/src/main/java/com/example/detch/projjiaxing_contacts_app/ContactProfile.java
+++ ProjJiaxing-Contacts-App/app/src/main/java/com/example/detch/projjiaxing_contacts_app/ContactProfile.java
@@ -1,13 +1,18 @@
 package com.example.detch.projjiaxing_contacts_app;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
@@ -39,7 +44,8 @@ public class ContactProfile extends AppCompatActivity {
     String relationshipsString;
     String[] relationshipNames;
     Bitmap viewPhoto;
-    byte[] encodedPhoto;
+    PhotoRepo repo; // database helper
+    boolean hasPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,22 +81,36 @@ public class ContactProfile extends AppCompatActivity {
                 ContactProfile.this,liteContactBook,R.layout.relationship_without_checkbox_per_item,new String[]{"name"},new int[]{R.id.watchRelationshipName});
         viewRelationships.setAdapter(viewRelationshipAdapter);
         viewRelationshipAdapter.notifyDataSetChanged();
+        // Now set database
+        repo = new PhotoRepo(this);
         // Now set profile photo
-        //Bitmap nullPhoto = BitmapFactory.decodeResource(getResources(),R.drawable.nullphoto);
-        encodedPhoto = this.getIntent().getByteArrayExtra("photo");
-        viewPhoto = BytesToBitmap(encodedPhoto);
+        ArrayList<String> namesWithPhoto = repo.getNames();
+        hasPhoto = false;
+        for (String nameWithPhoto : namesWithPhoto) {
+            if (this.name.equals(nameWithPhoto)) {
+                hasPhoto = true;
+            }
+        }
+        if (hasPhoto) {
+            Person viewedPerson = repo.getPersonByName(this.name);
+            this.viewPhoto = viewedPerson.getPhoto();
+            Log.e("SQLite","Found photo for " + this.name + ", loading photo.");
+        }
+        else {
+            this.viewPhoto = BitmapFactory.decodeResource(getResources(),R.drawable.nullphoto);
+            Log.e("SQLite","No photo for " + this.name + ", loading null-photo.");
+        }
         profilePhoto.setImageBitmap(viewPhoto);
-
-
-
 
         // Set listeners
         profilePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Start camera for a photo
-                Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(it, Activity.DEFAULT_KEYS_DIALER);
+                if (!hasPhoto) {
+                    // Start camera for a photo
+                    Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(it, Activity.DEFAULT_KEYS_DIALER);
+                }
             }
         });
     }
@@ -98,8 +118,8 @@ public class ContactProfile extends AppCompatActivity {
     @Override
     public void onBackPressed(){
         Intent doneViewing=new Intent();
-        doneViewing.putExtra("photo",this.encodedPhoto);
-        Log.e("Photo Trace","photo send back to main");
+        //doneViewing.putExtra("photo",this.encodedPhoto);
+        //Log.e("Photo Trace","photo send back to main");
         doneViewing.putExtra("name",this.name);
         ContactProfile.this.setResult(201,doneViewing);
         ContactProfile.this.finish();
@@ -116,14 +136,22 @@ public class ContactProfile extends AppCompatActivity {
             int squareWidth = w>=h?h:w;// Width after croping
             Bitmap croppedPhoto = Bitmap.createBitmap(rawPhoto,(w-squareWidth)/2,(h-squareWidth)/2,squareWidth,squareWidth);
             Bitmap photo = Bitmap.createScaledBitmap(croppedPhoto,200,200,true);
-            // Now show the photo (TEST)
+            rawPhoto = Bitmap.createScaledBitmap(croppedPhoto,1080,1080,true);
+            // Now show the photo
             profilePhoto.setImageBitmap(photo);
             this.viewPhoto = photo;
-            this.encodedPhoto = BitmapToBytes(photo);
-            Log.e("Photo Trace","taken photo updated to ContactProfile.java");
+            //this.encodedPhoto = BitmapToBytes(photo);
+            Log.e("Photo","Photo taken and resized.");
+            Person newFriend = new Person();
+            newFriend.name = this.name;
+            newFriend.setPhoto(this.viewPhoto);
+            newFriend.setRawPhoto(rawPhoto);
+            repo.insert(newFriend);
+            Log.e("SQLite","Added photo for " + this.name);
         } else {
             Toast.makeText(this.getApplicationContext(),
                     "Profile photo not added",Toast.LENGTH_SHORT).show();
+            Log.e("Photo", "Photo taking failed.");
         }
     }
 
@@ -138,6 +166,204 @@ public class ContactProfile extends AppCompatActivity {
             return BitmapFactory.decodeByteArray(b, 0, b.length);
         } else {
             return null;
+        }
+    }
+
+    public String BytesToBase64(byte[] bt) {
+        return Base64.encodeToString(bt,Base64.DEFAULT);
+    }
+
+    public byte[] Base64ToBytes(String st) {
+        return Base64.decode(st,Base64.NO_WRAP);
+    }
+
+
+    public class DBHelper extends SQLiteOpenHelper {
+
+        private static final int DATABASE_VERSION=3;
+        private static final String DATABASE_NAME="ContactAlbum.db";
+
+        public DBHelper(Context context){
+            super(context,DATABASE_NAME,null,DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            String CREATE_TABLE_ALBUM="CREATE TABLE "+ Person.TABLE+"("
+                    +Person.KEY_ID+" INTEGER PRIMARY KEY AUTOINCREMENT ,"
+                    +Person.KEY_name+" TEXT, "
+                    +Person.KEY_rawPhoto+" TEXT, "
+                    +Person.KEY_photo+" TEXT)";
+            db.execSQL(CREATE_TABLE_ALBUM);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS "+ Person.TABLE);
+            onCreate(db);
+        }
+    }
+
+    public class Person {
+        public static final String TABLE="album";
+
+        public static final String KEY_ID="id";
+        public static final String KEY_name="name";
+        public static final String KEY_photo="photo";
+        public static final String KEY_rawPhoto="rawphoto";
+
+        public int ID;
+        public String name;
+        public String photo;
+        public String rawPhoto;
+
+        public Person () {
+            this.ID = (int)System.currentTimeMillis();
+        }
+
+        public void setPhoto (Bitmap mappedPhoto) {
+            this.photo = BytesToBase64(BitmapToBytes(mappedPhoto));
+        }
+
+        public void setRawPhoto (Bitmap mappedPhoto) {
+            this.rawPhoto = BytesToBase64(BitmapToBytes(mappedPhoto));
+        }
+
+        public Bitmap getPhoto () {
+            return BytesToBitmap(Base64ToBytes(this.photo));
+        }
+
+        public Bitmap getRawPhoto () {
+            return BytesToBitmap(Base64ToBytes(this.rawPhoto));
+        }
+
+    }
+
+    public class PhotoRepo {
+        private DBHelper dbHelper;
+
+        public PhotoRepo(Context context){
+            dbHelper=new DBHelper(context);
+        }
+
+        public int insert(Person person){
+            SQLiteDatabase db=dbHelper.getWritableDatabase();
+            ContentValues values=new ContentValues();
+            values.put(Person.KEY_ID, person.ID);
+            values.put(Person.KEY_name, person.name);
+            values.put(Person.KEY_rawPhoto, person.rawPhoto);
+            values.put(Person.KEY_photo, person.photo);
+            long person_Id=db.insert(Person.TABLE,null,values);
+            db.close();
+            return (int)person_Id;
+        }
+
+        public void delete(int Id){
+            SQLiteDatabase db=dbHelper.getWritableDatabase();
+            db.delete(Person.TABLE,Person.KEY_ID+"=?", new String[]{String.valueOf(Id)});
+            db.close();
+        }
+        public void update (Person person) {
+            SQLiteDatabase db=dbHelper.getWritableDatabase();
+            ContentValues values=new ContentValues();
+            values.put(Person.KEY_name, person.name);
+            values.put(Person.KEY_photo, person.photo);
+            values.put(Person.KEY_rawPhoto, person.rawPhoto);
+            db.update(Person.TABLE,values,Person.KEY_ID+"=?",new String[] { String.valueOf(person.ID) });
+            db.close();
+        }
+
+        public ArrayList<HashMap<String, String>> getAll () {
+            SQLiteDatabase db=dbHelper.getReadableDatabase();
+            String selectQuery="SELECT "+
+                    Person.KEY_ID+","+
+                    Person.KEY_name+","+
+                    Person.KEY_rawPhoto+","+
+                    Person.KEY_photo+" FROM "+Person.TABLE;
+            ArrayList<HashMap<String,String>> list=new ArrayList<HashMap<String, String>>();
+            Cursor cursor=db.rawQuery(selectQuery,null);
+            if(cursor.moveToFirst()){
+                do{
+                    HashMap<String,String> person=new HashMap<String,String>();
+                    //person.put("id",cursor.getString(cursor.getColumnIndex(Person.KEY_ID)));
+                    person.put("name",cursor.getString(cursor.getColumnIndex(Person.KEY_name)));
+                    person.put("photo",cursor.getString(cursor.getColumnIndex(Person.KEY_photo)));
+                    person.put("rawPhoto",cursor.getString(cursor.getColumnIndex(Person.KEY_rawPhoto)));
+                    list.add(person);
+                } while(cursor.moveToNext());
+            }
+            cursor.close();
+            db.close();
+            return list;
+        }
+
+        public Person getPersonById(int Id){
+            SQLiteDatabase db=dbHelper.getReadableDatabase();
+            String selectQuery="SELECT "+
+                    Person.KEY_ID + "," +
+                    Person.KEY_name + "," +
+                    Person.KEY_rawPhoto + "," +
+                    Person.KEY_photo +
+                    " FROM " + Person.TABLE
+                    + " WHERE " +
+                    Person.KEY_ID + "=?";
+            Person person = new Person();
+            Cursor cursor=db.rawQuery(selectQuery,new String[]{String.valueOf(Id)});
+            if(cursor.moveToFirst()){
+                do{
+                    person.ID = cursor.getInt(cursor.getColumnIndex(Person.KEY_ID));
+                    person.name = cursor.getString(cursor.getColumnIndex(Person.KEY_name));
+                    person.photo  = cursor.getString(cursor.getColumnIndex(Person.KEY_photo));
+                    person.rawPhoto  = cursor.getString(cursor.getColumnIndex(Person.KEY_rawPhoto));
+                }while(cursor.moveToNext());
+            }
+            cursor.close();
+            db.close();
+            return person;
+        }
+
+        public Person getPersonByName(String name){
+            Log.e("SQLite", "Get Person by name: " + name);
+            SQLiteDatabase db=dbHelper.getReadableDatabase();
+            String selectQuery="SELECT "+
+                    Person.KEY_ID + "," +
+                    Person.KEY_name + "," +
+                    Person.KEY_rawPhoto + "," +
+                    Person.KEY_photo +
+                    " FROM " + Person.TABLE
+                    + " WHERE " +
+                    Person.KEY_name + "=?";
+            Person person = new Person();
+            Cursor cursor=db.rawQuery(selectQuery, new String[]{name});
+            if(cursor.moveToFirst()){
+                do{
+                    person.ID = cursor.getInt(cursor.getColumnIndex(Person.KEY_ID));
+                    person.name = cursor.getString(cursor.getColumnIndex(Person.KEY_name));
+                    person.photo  = cursor.getString(cursor.getColumnIndex(Person.KEY_photo));
+                    person.rawPhoto  = cursor.getString(cursor.getColumnIndex(Person.KEY_rawPhoto));
+                }while(cursor.moveToNext());
+            }
+            cursor.close();
+            db.close();
+            return person;
+        }
+
+        public ArrayList<String> getNames () {
+            SQLiteDatabase db = dbHelper.getReadableDatabase();
+            String selectQuery="SELECT "+
+                    Person.KEY_ID+","+
+                    Person.KEY_name+","+
+                    Person.KEY_photo+" FROM "+Person.TABLE;
+            ArrayList<String> list = new ArrayList<String>();
+            Cursor cursor=db.rawQuery(selectQuery,null);
+            if(cursor.moveToFirst()){
+                do{
+                    list.add(cursor.getString(cursor.getColumnIndex(Person.KEY_name)));
+                }while(cursor.moveToNext());
+            }
+            cursor.close();
+            db.close();
+            return list;
         }
     }
 }
--- ProjJiaxing-Contacts-App/app/src/main/java/com/example/detch/projjiaxing_contacts_app/ContactsList.java
+++ ProjJiaxing-Contacts-App/app/src/main/java/com/example/detch/projjiaxing_contacts_app/ContactsList.java
@@ -1,8 +1,11 @@
 package com.example.detch.projjiaxing_contacts_app;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
@@ -35,7 +38,7 @@ public class ContactsList extends AppCompatActivity {
     List<Map<String,String >> contactbook = new ArrayList<Map<String,String >>();//contacts and their relationships are saved here
     static List<Integer> to_delete = new ArrayList<Integer>();//index to delete
     ContactListMainListAdapter contactListAdapter;
-    Map<String,byte[]> contactAlbum = new HashMap<String, byte[]>();
+    //Map<String,byte[]> contactAlbum = new HashMap<String, byte[]>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,25 +97,23 @@ public class ContactsList extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
                 Map<String,String> personToShow = contactbook.get(position);
-                byte[] photoToShow;
+                //byte[] photoToShow;
+                //if (contactAlbum.containsKey(nameToShow)) {
+                //    photoToShow = contactAlbum.get(nameToShow);
+                //    Log.e("Photo Trace","found photo for "+nameToShow+", sending to profile");
+                //} else {
+                //    Log.e("Photo Trace","cannot find photo for "+nameToShow+"using default");
+                //    Bitmap nullPhoto = BitmapFactory.decodeResource(getResources(),R.drawable.nullphoto);
+                //    photoToShow = BitmapToBytes(nullPhoto);
+                //    contactAlbum.put(nameToShow,photoToShow);
+                //}
                 String nameToShow = personToShow.get("name");
-                if (contactAlbum.containsKey(nameToShow)) {
-                    photoToShow = contactAlbum.get(nameToShow);
-                    Log.e("Photo Trace","found photo for "+nameToShow+", sending to profile");
-                } else {
-                    Log.e("Photo Trace","cannot find photo for "+nameToShow+"using default");
-                    Bitmap nullPhoto = BitmapFactory.decodeResource(getResources(),R.drawable.nullphoto);
-                    photoToShow = BitmapToBytes(nullPhoto);
-                    contactAlbum.put(nameToShow,photoToShow);
-                }
                 Intent personInfo = new Intent(ContactsList.this,ContactProfile.class);
                 personInfo.putExtra("name",nameToShow);
                 personInfo.putExtra("phone",personToShow.get("phone"));
                 personInfo.putExtra("relationships",personToShow.get("relationships"));
-                personInfo.putExtra("photo",photoToShow);
+                //personInfo.putExtra("photo",photoToShow);
                 startActivityForResult(personInfo,200);
-                //startActivity(personInfo);
-                //saveData(ContactsList.this);
             }
         });
     }
@@ -188,80 +189,16 @@ public class ContactsList extends AppCompatActivity {
                 break;
             case 201:
                 // Done watching profile details
-                byte[] encodedPhoto = data.getByteArrayExtra("photo");
-                String viewedName = data.getStringExtra("name");
-                this.contactAlbum.put(viewedName,encodedPhoto);
-                Log.e("Photo Trace","photo received in main and updated in main");
+                //byte[] encodedPhoto = data.getByteArrayExtra("photo");
+                //String viewedName = data.getStringExtra("name");
+                //this.contactAlbum.put(viewedName,encodedPhoto);
+                Log.d("Main result", "Safely back from profile viewing.");
                 saveData(ContactsList.this);
                 break;
             default:
                 break;
         }
-        /*
-        if(resultCode==101){
-            // Case: just added new contact
-            // Now fetch intent data
-            Bundle newlyAdded=data.getExtras();
-            String newName=newlyAdded.getString("name");
-            String newPhone=newlyAdded.getString("phone");
-            ArrayList<String> newRelationship=newlyAdded.getStringArrayList("relationship");
-            StringBuffer relationshipBuffer=new StringBuffer();
-            for(int i=0;i<newRelationship.size();i++){
-                relationshipBuffer.append(newRelationship.get(i));
-                relationshipBuffer.append(";");
-            }
-            if(relationshipBuffer.length()>0){
-                relationshipBuffer.deleteCharAt(relationshipBuffer.length()-1);
-            }
-            // Now create a new object(person) for contactbook
-            Map<String,String> newPerson=new HashMap<String, String>();
-            newPerson.put("name",newName);
-            newPerson.put("phone",newPhone);
-            newPerson.put("relationships",newRelationship.toString());
-            this.contactbook.add(newPerson);
-            // Now add this new person's relationship to existing contacts (two-way relationship)
-            for(int i=0;i<newRelationship.size();i++){
-                String oldFriendName = newRelationship.get(i);
-                // for (Map<String,String> oldFriend : this.contactbook){
-                for (int j=0;j<this.contactbook.size();j++){
-                    Map<String,String> oldFriend = this.contactbook.get(j);
-                    if (oldFriend.get("name").equals(oldFriendName)) {
-                        String updatedRelationships = new String();
-                        if (oldFriend.get("relationships").length()<3){// No relationship yet
-                            updatedRelationships = "["+newName+"]";
-                        } else {// Append newName to end of String
-                            StringBuffer oldRelationshipBuffer = new StringBuffer(oldFriend.get("relationships"));
-                            oldRelationshipBuffer.deleteCharAt(oldRelationshipBuffer.length()-1);
-                            updatedRelationships = oldRelationshipBuffer + ", " + newName + "]";
-                        }
-                        Log.e("Update old relationship"+oldFriendName,"Now relation:"+updatedRelationships);
-                        oldFriend.put("relationships",updatedRelationships);
-                        this.contactbook.set(j,oldFriend);// Update oldFriend back
-                    }
-                }
-            }
-            // Now do some wrap-ups
-            Log.e("Activity Jump","Added new contact");
-            Toast.makeText(ContactsList.this.getApplicationContext(),
-                    "Added "+newName,Toast.LENGTH_SHORT).show();
-            this.contactListAdapter.notifyDataSetChanged();
-            saveData(ContactsList.this);
-        }
-        else if(requestCode==102){
-            // Canceled adding contact
-            Toast.makeText(ContactsList.this.getApplicationContext(),
-                    "Canceled adding",Toast.LENGTH_SHORT).show();
-        }
-        else if(requestCode==201){
-            // Done watching profile details
-            byte[] encodedPhoto = data.getByteArrayExtra("photo");
-            String viewedName = data.getStringExtra("name");
-            this.contactAlbum.put(viewedName,encodedPhoto);
-            Log.e("Photo Trace","photo received in main and updated in main");
-            saveData(ContactsList.this);
-        }
-        else{}
-        */
+
 
     }
 
@@ -287,6 +224,7 @@ public class ContactsList extends AppCompatActivity {
         SharedPreferences.Editor editor = sp.edit();
         editor.putString(key, mJsonArray.toString());
         editor.commit();
+        /*
         // Now encode contactAlbum photo(byte[]) into string(Base64) and Save
         Map<String, String> encodedAlbum = new HashMap<String, String>();
         List<Map<String,String>> data2 = new ArrayList<Map<String, String>>();
@@ -310,6 +248,7 @@ public class ContactsList extends AppCompatActivity {
         SharedPreferences.Editor editor2 = sp2.edit();
         editor2.putString(key2, mJsonArray2.toString());
         editor2.commit();
+        */
     }
 
     public void loadData(Context context) {
@@ -335,6 +274,7 @@ public class ContactsList extends AppCompatActivity {
             }
         } catch (JSONException e) { }
         this.contactbook=data;
+        /*
         // Now restore contact album
         String key2 = "contact album saves";
         Map<String, String> encodedAlbum = new HashMap<String, String>();
@@ -360,6 +300,7 @@ public class ContactsList extends AppCompatActivity {
             byte[] decodedAlbumPhoto = Base64ToBytes(encodedAlbum.get(encodedAlbumKey));
             this.contactAlbum.put(encodedAlbumKey,decodedAlbumPhoto);
         }
+        */
     }
 
     public byte[] BitmapToBytes(Bitmap bm) {
